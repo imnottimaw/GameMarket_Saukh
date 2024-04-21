eff078c8b335893b09d6548f95f573243660a8b7
Tymofiy Saukh <23b_stv@liceum.ztu.edu.ua>
2024-04-21 21:30:19 +0300

    PJcdprjtgXcZgxrQkCMuyYNEOdnMurmImMsoeXycqaVzvkjoveCrRHlzFQGsXFOD

---
+++ script.js
@@ -0,0 +1,29 @@
+let c94EKu1LsgG4miYw60AzslDVEoRFiz29SYIObDA8BWitXBfymwqGY8yBIfKUFNj5 = 1;
+yjxEEpbaPjTvFWHvkkXuHLqrbWbiyiATsOwTQVJFZTEcHEZaLkogXiOoNJiwRBIi(c94EKu1LsgG4miYw60AzslDVEoRFiz29SYIObDA8BWitXBfymwqGY8yBIfKUFNj5);
+
+function utTMuGaKCcDolyWNUjCXrySkWgAZQNowDRlEYYKTtanNrUrlttZGvnGniHSSOnZs(zuo4lXiSoarWSAiwnO32m1zv3TKW0C290ckYItEDhGPZOOdtMmQGrOadj8a0xmzK) {
+  yjxEEpbaPjTvFWHvkkXuHLqrbWbiyiATsOwTQVJFZTEcHEZaLkogXiOoNJiwRBIi(c94EKu1LsgG4miYw60AzslDVEoRFiz29SYIObDA8BWitXBfymwqGY8yBIfKUFNj5 += zuo4lXiSoarWSAiwnO32m1zv3TKW0C290ckYItEDhGPZOOdtMmQGrOadj8a0xmzK);
+}
+
+function yjxEEpbaPjTvFWHvkkXuHLqrbWbiyiATsOwTQVJFZTEcHEZaLkogXiOoNJiwRBIi(zuo4lXiSoarWSAiwnO32m1zv3TKW0C290ckYItEDhGPZOOdtMmQGrOadj8a0xmzK) {
+  let irFI5uOsNubiyJDu7wjEDmAccvq1dVluKb1s56A0oXLo61JBwh0CZ1Zfrv60BQ58;
+  let LeLwyCIxjJESyVABAFiJTraLiPjpqqGlIPh9e84LOQrk4Ww87QeNm2va1tTuMNR5 = document.getElementsByClassName("Om1sGFCLKkZ5FaoW5cIshuPdiIjdYB59j9VA7obenAFPk3AKVImEEA8R3azNjTce");
+  if (zuo4lXiSoarWSAiwnO32m1zv3TKW0C290ckYItEDhGPZOOdtMmQGrOadj8a0xmzK > LeLwyCIxjJESyVABAFiJTraLiPjpqqGlIPh9e84LOQrk4Ww87QeNm2va1tTuMNR5.length) {c94EKu1LsgG4miYw60AzslDVEoRFiz29SYIObDA8BWitXBfymwqGY8yBIfKUFNj5 = 1}
+  if (zuo4lXiSoarWSAiwnO32m1zv3TKW0C290ckYItEDhGPZOOdtMmQGrOadj8a0xmzK < 1) {c94EKu1LsgG4miYw60AzslDVEoRFiz29SYIObDA8BWitXBfymwqGY8yBIfKUFNj5 = LeLwyCIxjJESyVABAFiJTraLiPjpqqGlIPh9e84LOQrk4Ww87QeNm2va1tTuMNR5.length}
+  for (irFI5uOsNubiyJDu7wjEDmAccvq1dVluKb1s56A0oXLo61JBwh0CZ1Zfrv60BQ58 = 0; irFI5uOsNubiyJDu7wjEDmAccvq1dVluKb1s56A0oXLo61JBwh0CZ1Zfrv60BQ58 < LeLwyCIxjJESyVABAFiJTraLiPjpqqGlIPh9e84LOQrk4Ww87QeNm2va1tTuMNR5.length; irFI5uOsNubiyJDu7wjEDmAccvq1dVluKb1s56A0oXLo61JBwh0CZ1Zfrv60BQ58++) {
+    LeLwyCIxjJESyVABAFiJTraLiPjpqqGlIPh9e84LOQrk4Ww87QeNm2va1tTuMNR5[irFI5uOsNubiyJDu7wjEDmAccvq1dVluKb1s56A0oXLo61JBwh0CZ1Zfrv60BQ58].style.display = "none";
+  }
+  LeLwyCIxjJESyVABAFiJTraLiPjpqqGlIPh9e84LOQrk4Ww87QeNm2va1tTuMNR5[c94EKu1LsgG4miYw60AzslDVEoRFiz29SYIObDA8BWitXBfymwqGY8yBIfKUFNj5-1].style.display = "block";
+}
+function SyvbsLsucEVzKxWBSZosGdcEmmAksXEAEUTwLgDOGXtQwcQCODgRlIIdzAiMHcgJ(){
+    window.open("https://github.com/imnottimaw/Phaser2ndGame",'_blank').focus();
+}
+function WKOQyRDzJsLMRttZsppilRJgtmLZPIEYpKNYQBFChgtaGTYxgpabinUSusdigTPO(){
+    window.open("https://imnottimaw.github.io/Phaser2ndGame",'_blank').focus();
+}
+function eZdxBsJRgKQYxwtTznkWtuQrqcrezReYeQtEkqAbZHJLKrpTiZTFblywIpBhpEEx(){
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLScvpkH4jhI659WHxOMafwAVBq2gMDgc_bcIo-1uJx2aueZhxw/viewform",'_blank').focus();
+}
+function lsvVjzasXMvWTiKwLhfSnSLvaruLCeuhznFxbzQyWHySfvaimqWhabTLHFPefGWy(zuo4lXiSoarWSAiwnO32m1zv3TKW0C290ckYItEDhGPZOOdtMmQGrOadj8a0xmzK){
+  alert("Ваш відгук надіслано ("+zuo4lXiSoarWSAiwnO32m1zv3TKW0C290ckYItEDhGPZOOdtMmQGrOadj8a0xmzK+")")
+}
